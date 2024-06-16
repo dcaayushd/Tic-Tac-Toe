0f205194f710efc524714429d9927299131fe35d
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-06-17 01:35:41 +0545

    User Model Added

---
+++ lib/models/user_model.dart
@@ -0,0 +1,44 @@
+class UserModel {
+  String? id;
+  String? name;
+  String? email;
+  String? image;
+  String? totalWins;
+
+  UserModel({
+    this.id,
+    this.name,
+    this.email,
+    this.image,
+    this.totalWins,
+  });
+
+  UserModel.fromJson(Map<String, dynamic> json) {
+    if (json["id"] is String) {
+      id = json["id"];
+    }
+    if (json["name"] is String) {
+      name = json["name"];
+    }
+    if (json["email"] is String) {
+      email = json["email"];
+    }
+    if (json["image"] is String) {
+      image = json["image"];
+    }
+    if (json["totalWins"] is String) {
+      totalWins = json["totalWins"];
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> _data = <String, dynamic>{};
+    _data["id"] = id;
+    _data["name"] = name;
+    _data["email"] = email;
+    _data["image"] = image;
+    _data["totalWins"] = totalWins;
+
+    return _data;
+  }
+}
