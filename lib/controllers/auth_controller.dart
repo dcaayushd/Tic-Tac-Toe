659e3a48e29b5e7f338b074a6224e35bfd9a5f6f
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-06-17 01:34:21 +0545

    Auth Controller Added

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,13 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
-import 'package:tic_tac_toe/screens/home/home_screen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:tic_tac_toe/models/user_model.dart';
 import 'package:tic_tac_toe/screens/update_profile/update_profile_screen.dart';
 
+import '../configs/Messages.dart';
+import '../screens/home/home_screen.dart';
+
 class AuthController extends GetxController {
+  final auth = FirebaseAuth.instance;
+  final db = FirebaseFirestore.instance;
   Future<void> login() async {
-    Get.to(UpdateProfileScreen());
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+      await auth.signInWithCredential(credential);
+      successMessage("Login Success");
+      Get.to(() => UpdateProfileScreen());
+    } catch (e) {
+      errorMessage("Login Failed");
+      print(e);
+    }
   }
 
-  Future<void> updateProfile() async {
-    Get.to(HomeScreen());
+  Future<void> updateProfile(String name, String imagePath) async {
+    var newUser = UserModel(
+      id: auth.currentUser!.uid,
+      name: name,
+      image: imagePath,
+      email: auth.currentUser!.email,
+      totalWins: '0',
+    );
+    await db.collection('users').doc(auth.currentUser!.uid).set(
+          newUser.toJson(),
+        );
+
+    Get.to(() => HomeScreen());
   }
 }
