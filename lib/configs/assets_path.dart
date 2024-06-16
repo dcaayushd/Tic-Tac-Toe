95406c710e2daaf7cade06327af7e35a1dbcbf5e
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-06-16 20:14:10 +0545

    Icons Imported & Paths Defined

--- lib/configs/assets_path.dart
+++ lib/configs/assets_path.dart
@@ -8,4 +8,30 @@ class IconsPath {
   static const xIcon = '${basePath}x.svg';
   static const wonIcon = '${basePath}won.svg';
   static const msgIcon = '${basePath}sms.svg';
+
+  static const appLogo = "${basePath}appLogo.svg";
+  static const mic = "${basePath}mic.svg";
+  static const camera = "${basePath}camera.svg";
+  static const gallery = "${basePath}gallery.svg";
+  static const game = "${basePath}game.svg";
+  static const github = "${basePath}github.svg";
+  static const google = "${basePath}google.svg";
+  static const group = "${basePath}group.svg";
+  static const info = "${basePath}info.svg";
+  static const save = "${basePath}save.svg";
+  static const user = "${basePath}user.svg";
+  static const youtube = "${basePath}youtube.svg";
 }
+
+
+class ImagePath {
+  static const basePath = "assets/photos/";
+  static const boy = "${basePath}boy.png";
+  static const girl = "${basePath}girl.png";
+  static const welcome1 = "${basePath}welcome1.png";
+  static const welcome2 = "${basePath}welcome2.png";
+  static const welcome3 = "${basePath}welcome3.png";
+}
+
+
+ const String defaultImage = "https://www.pngall.com/wp-content/uploads/5/Profile.png";
