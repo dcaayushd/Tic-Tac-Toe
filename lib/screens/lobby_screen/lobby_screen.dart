b8a50aefdc040427e8e4efe44f8f215e8fe4f6cd
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-06-16 11:42:51 +0545

    User Card Widget Added

--- lib/screens/lobby_screen/lobby_screen.dart
+++ lib/screens/lobby_screen/lobby_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:tic_tac_toe/screens/lobby_screen/widgets/pricing_area.dart';
 import 'package:tic_tac_toe/screens/lobby_screen/widgets/room_info.dart';
+import 'package:tic_tac_toe/widgets/primary_button.dart';
+import 'package:tic_tac_toe/widgets/user_card.dart';
 // import 'package:get/get.dart';
 
 import '../../configs/assets_path.dart';
@@ -18,32 +20,39 @@ class LobbyScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  SvgPicture.asset(IconsPath.backIcon),
-                  SizedBox(
-                    width: 15.0,
-                  ),
-                  Text(
-                    'Play With Private Room',
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              RoomInfo(roomCode: '123456'),
-              SizedBox(
-                height: 40,
-              ),
-              PriceArea(entryPrice: '23', winningPrice: '46'),
-              SizedBox(
-                height: 50,
-              ),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    SvgPicture.asset(IconsPath.backIcon),
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Text(
+                      'Play With Private Room',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                RoomInfo(roomCode: '123456'),
+                SizedBox(
+                  height: 40,
+                ),
+                PriceArea(entryPrice: '23', winningPrice: '46'),
+                SizedBox(height: 90),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    UserCard(),
+                    UserCard(),
+                  ],
+                ),
+                SizedBox(height: 20),
+                PrimaryButton(buttonText: 'Start Game', onTap: () {})
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/widgets/user_card.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../configs/assets_path.dart';
+
+class UserCard extends StatefulWidget {
+  const UserCard({super.key});
+
+  @override
+  State<UserCard> createState() => _UserCardState();
+}
+
+class _UserCardState extends State<UserCard> {
+  @override
+  Widget build(BuildContext context) {
+    final w = MediaQuery.of(context).size.width;
+
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          width: w / 2.6,
+          height: 140,
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 60),
+              Text(
+                'Aayush Dc',
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    IconsPath.coinIcon,
+                    width: 20,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    '23 Coins',
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          top: -50,
+          left: w / 2.6 / 2 - 50,
+          child: Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.circular(100),
+                border: Border.all(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  width: 3,
+                )),
+          ),
+        ),
+      ],
+    );
+  }
+}
