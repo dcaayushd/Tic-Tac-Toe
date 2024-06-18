c0829d57621d385192abd5ed18380881756bd555
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-06-18 21:38:53 +0545

    Check Winner Function Implemented

--- lib/controllers/multi_player_controller.dart
+++ lib/controllers/multi_player_controller.dart
@@ -40,7 +40,146 @@ class MultiPlayerController extends GetxController {
       }
     }
 
-
+    checkWinner(oldValue, roomData);
   }
 
+  void checkWinner(List<String> playValue, RoomModel roomData) {
+    // Vertical
+    if (playValue[0] == playValue[1] &&
+        playValue[0] == playValue[2] &&
+        playValue[0] != "") {
+      print("winner is ${playValue[0]}");
+      winnerDialog(playValue[0], roomData);
+    } else if (playValue[3] == playValue[4] &&
+        playValue[3] == playValue[5] &&
+        playValue[3] != "") {
+      winnerDialog(playValue[3], roomData);
+      print("winner is ${playValue[3]}");
+    } else if (playValue[6] == playValue[7] &&
+        playValue[6] == playValue[8] &&
+        playValue[6] != "") {
+      print("winner is ${playValue[6]}");
+      winnerDialog(playValue[6], roomData);
+    }
+    // Horizontal
+    else if (playValue[0] == playValue[3] &&
+        playValue[0] == playValue[6] &&
+        playValue[0] != "") {
+      winnerDialog(playValue[0], roomData);
+      print("winner is ${playValue[0]}");
+    } else if (playValue[1] == playValue[4] &&
+        playValue[1] == playValue[7] &&
+        playValue[1] != "") {
+      winnerDialog(playValue[1], roomData);
+      print("winner is ${playValue[1]}");
+    } else if (playValue[2] == playValue[5] &&
+        playValue[2] == playValue[8] &&
+        playValue[2] != "") {
+      winnerDialog(playValue[2], roomData);
+      print("winner is ${playValue[2]}");
+      // digobalss
+    } else if (playValue[0] == playValue[4] &&
+        playValue[0] == playValue[8] &&
+        playValue[0] != "") {
+      winnerDialog(playValue[0], roomData);
+      print("winner is ${playValue[0]}");
+    } else if (playValue[2] == playValue[4] &&
+        playValue[2] == playValue[6] &&
+        playValue[2] != "") {
+      print("winner is ${playValue[2]}");
+      winnerDialog(playValue[2], roomData);
+    } else {
+      if (!playValue.contains("")) {
+        winnerDialog("no-one", roomData);
+      }
+    }
+  }
+
+  Future<dynamic> winnerDialog(String winner, RoomModel roomData) {
+    // scoreCalculate(winner);
+    return Get.defaultDialog(
+        barrierDismissible: false,
+        title: winner == "no-one" ? "Match Draw" : "Congratulations",
+        backgroundColor: Colors.white,
+        content: Padding(
+          padding: const EdgeInsets.all(10),
+          child: winner == "no-one"
+              ? Column(
+                  children: [
+                    const Text(
+                      "Match Draw",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    const Text(
+                      "You both played well",
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+
+                          },
+                          child: Text("Play Again"),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Get.offAllNamed("/home");
+                          },
+                          child: Text("Exit"),
+                        )
+                      ],
+                    )
+                  ],
+                )
+              : Column(
+                  children: [
+                    SvgPicture.asset(
+                      IconsPath.wonIcon,
+                      width: 100,
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "Congratulations",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text(
+                      "$winner won the match",
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+
+                            Get.back();
+                          },
+                          child: Text("Play Again"),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            Get.offAllNamed("/home");
+                          },
+                          child: Text("Exit"),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+        ));
+  }
+
+  
 }
